8768dc9ce24620f18a5fd24672c491d6995320bf
g.rokah@yandex.ru <g.rokah@yandex.ru>
2022-03-21 14:06:36 +0300

    added sql connection and created some tables

---
+++ Lesson3_18_03_2022/postgreJDBC/src/main/java/JDBCPostgreSQLExample.java
@@ -0,0 +1,83 @@
+package com.vertex.academy.databases;
+
+//STEP 1. Import required packages
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCPostgreSQLExample {
+
+    //  Database credentials
+    static final String DB_URL ="jdbc:postgresql://localhost:5433/testbase";
+    static final String USER = "postgres";
+    static final String PASS = "3355";
+
+
+    public static void main(String[] argv) {
+        Statement stmt = null;
+        System.out.println("Testing connection to PostgreSQL JDBC");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("PostgreSQL JDBC Driver successfully connected");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection(DB_URL, USER, PASS);
+            Statement statement = connection.createStatement();
+            stmt = connection.createStatement();
+
+
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
+
+
+
+
+            stmt.close();
+            connection.close();
+
+
+
+
+
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You successfully connected to database now");
+        } else {
+            System.out.println("Failed to make connection to database");
+        }
+
+
+
+
+    }
+
+}
